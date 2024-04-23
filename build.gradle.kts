dc4f335ef81bfec83e0e0da377715983ffb1e302
nasamio <nasamio@163.com>
2024-04-23 16:46:19 +0800

    补充提交，解决jitpack编译不过的问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    //必须添加
+    dependencies {
+        classpath ("com.github.dcendents:android-maven-gradle-plugin:2.1")
+    }
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0-rc03" apply false
